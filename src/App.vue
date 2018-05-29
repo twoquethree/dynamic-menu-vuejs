34a0591f1a16293d3aafdfb51432f40fc55b8602
Diego Melgar <dlopez.project.sv@gmail.com>
2018-05-28 19:03:45 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fill-height fluid>
+          <v-layout 
+          justify-center
+          align-center
+          hidden-xs-only
+          column>
+           <v-card color="darken-2" class="elevation-3">
+              <v-card-title primary-title>
+                <div class="subheading">Unlimited music now</div>
+              </v-card-title>
+               <v-list>
+                  <v-list-tile v-for="n in 5" :key="n">
+                    <v-list-tile-title v-text="'Item ' + n"></v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+            </v-card>
+          </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
